aa4423e74716e5a76c6b827da392a24549ab4ed2
darkarnold <arnoldndiwalana@gmail.com>
2021-11-14 04:12:05 +0300

    fix: import files

--- util.php
+++ util.php
@@ -1,7 +1,7 @@
 <?php 
 class Util {
   static $GO_TO_MAIN_MENU = '00';
-  static $API_KEY = 'API_KEY';
+  static $API_KEY = 'ee9ee94ddc275ece14f6ca255a61fcbf9ebd199ee23040b47171d677258e29a2';
   static $API_USERNAME ='sandbox';
   static $COMPANY_NAME = 'Wildlife Authority'; 
 }
